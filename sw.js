71df439d7b27010f1f4d09805f21b7367e74807f
Chaitanyaagra <chaitanyaagrawal77@gmail.com>
2026-09-08 12:48:16 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    Bump CACHE_NAME on any future structural change to force a clean cache.
    ========================================================================== */
 
-const CACHE_NAME = 'mhmrws-shell-v41';
+const CACHE_NAME = 'mhmrws-shell-v42';
 const SHELL_FILES = [
   './',
   './index.html',
